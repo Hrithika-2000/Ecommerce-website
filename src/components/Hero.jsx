ea29f3622dcdd8d566bbff2632a4f91922fd74a3
Hrithika-2000 <hrithikafoxiom@gmail.com>
2025-01-22 16:34:05 +0530

    commit changes

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col sm:flex-row border border-gray-400'>
+        {/* Hero Left Sidebar */}
+        <div className=''></div>
+    </div>
+  )
+}
+
+export default Hero
